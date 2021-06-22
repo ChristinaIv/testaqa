f3ef203e7190c9328737986b7ba96981288fb26a
Christina <kristiya2304@gmail.com>
2021-06-22 19:55:37 +0300

    first

---
+++ project.js
@@ -0,0 +1,10 @@
+let username,age,massage;
+username = "Kate",age = 12, massage = 'Holla!';
+console.log(username);
+console.log(age);
+console.log(massage);
+const user = {
+    name: `Вася Иванов`
+};
+console.log(user.name);
+
