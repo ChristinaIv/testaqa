03f82feea8f3912155113dd982d98aa289f6ce5c
Christina <kristiya2304@gmail.com>
2021-06-30 12:55:13 +0300

    first task

---
+++ task1_v1.js
@@ -0,0 +1,35 @@
+let a = 10;
+console.log(a += 3);
+
+let aa = 4;
+aa ++;
+console.log(aa);
+
+let b = 'хочу'
+console.log(b += ' в отпуск');
+
+let c = 30 - 2;
+console.log(c);
+
+let bet = 3 ;
+bet--;
+console.log(bet);
+
+let d = (100 + 200) * 5;
+console.log(d);
+
+let e = 5 * 2;
+console.log(e);
+
+let f = 60 , g = 3;
+let h = f / g;
+console.log(h);
+
+let k = 70 , l = 3;
+let del = 70 % 3;
+console.log(del);
+
+console.log(Number('30') * Number('2'));
+
+
+
