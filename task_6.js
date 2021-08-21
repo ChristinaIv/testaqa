667159ece9cdd91b4d6e91d922f1972db15b3145
Christina <kristiya2304@gmail.com>
2021-08-21 17:17:20 +0300

    change_functions

--- task_6.js
+++ task_6.js
@@ -1,35 +1,44 @@
 function sumAB(a,b) {return a+b};
-sumAB (1,2);
+sumAB (1,2);                  //сумма двух чисел
 
 
-function sum(a){
-    return function(b){
-        return a + b;
-    }
-}
 
-console.log(sum(5)(6));         //сумма двух чисел
-
-function checkName(name) {
+function checkName() {
+    const name = prompt("Enter name");
     if (name === 'Christina') {
         return true;
-    }
+    } else {return false}
+}
+if ( checkName() ) alert('Hello' + ' ' + 'Christina'); else {
+    alert( 'Mistake' );
 }
 
-let name = prompt('Enter name');
-
-if ( checkName(name) ) {
-    alert( 'Hello' + ' ' + name );
-} else {
-    alert( 'Mistake' );
-}                                                   // проверка имени
+function checkName() {
+    let userName
+    {
+      username = prompt('Your name?')
+        if (userName === 'Christina')     {
+            return console.log('Hello Christina')
+        }    else {
+            return console.log('Ha20llo')
+        }
+    }                                               // проверка имени
 
 
 const onlyEven = arr => arr.filter(x => x % 2 === 0)
 
 const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-console.log(onlyEven(data))            // вывод только четных
+console.log(onlyEven(data))
+
+function filter.data{
+    const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    data.filter(x => x % 2 === 0);
+
+                                                   // вывод только четных
+}
+console.log(filter.data)
+
 
 function typeOfElement (z) {
     return typeof(z)
@@ -41,7 +50,11 @@ console.log (typeOfElement('true'))  // вычисление типа аргум
 function checkAge(age) {
     return (age > 18) || confirm('Родители разрешили?');
 }
-console.log (checkAge())
+console.log (checkAge(12));
+
+
+function checkAge(age) {
+    return (age > 18) ? true : confirm('Родители разрешили?');
 
 function min(a, b) {
     if (a < b) {
@@ -65,6 +78,16 @@ function alphabet_order(str)
 }
 console.log(alphabet_order("webmaster"));
 
+
+function Longest_Country_Name(country_name)
+{
+    return country_name.reduce(function(lname, country)
+        {
+            return lname.length > country.length ? lname : country;
+        }
+        )
+}
+console.log(Longest_Country_Name(["Australia", "Germany", "United States of America"]))
 
 
 
