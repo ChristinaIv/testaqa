b2b62f1646ee917c7f97a53ef162b08e62f43071
Christina <kristiya2304@gmail.com>
2021-09-11 10:38:43 +0300

    task_7_changed

--- task_7.js
+++ task_7.js
@@ -7,13 +7,13 @@ console.log(document.querySelector('[href="https://www.bbc.com/reel"]'))
 a = $x('//*[@id="orb-nav-links"]/ul/li[4]/a')                                     //ссылка через XPath
 
 console.log(document.querySelector("a.block-link__overlay-link"))     //первое изображение на главной странице
-a = $x ('//*[@id="page"]/section[3]/div/ul/li[1]/div/a')                     // тоже изображение на главной странице через XPath
+a = $x ('//*[@class="media__link"]/section[3]/div/ul/li[1]/div/a')                    // тоже изображение на главной странице через XPath
 
 console.log(document.querySelector("li.media-list__item.media-list__item--2"))  //изображение 2 на главной странице
-a = $x ('//*[@id="page"]/section[3]/div/ul/li[2]/div/a')                //  изображение 2 через XPath
+a = $x ('//*[@class="media-list__item media-list__item--2"]/section[3]/div/ul/li[2]/div/a')                //  изображение 2 через XPath
 
 console.log(document.querySelector('#orb-nav-links').querySelectorAll("li:nth-child(even)>a")) // четные разделы
-                                                                     //как работает через XPath непонятно
+div[@id="orb-nav-links"]//li[not(contains(@class, "orb-nav-hide"))][position() mod 2 = 0]/a        // через XPath
 
 console.log(document.querySelector("h2.module__title"))          //дата
-a = $x ('//*[@id="page"]/section[2]/h2')                                 //дата XPath
+a = $x ('//*[@class="module__title"]/section[2]/h2')                                 //дата XPath
